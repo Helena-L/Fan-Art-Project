e660ff434aeb367ca0d5f9faafb399cb5f81b973
sweinstein22 <sweinstein22@berkeley.edu>
2016-11-06 14:03:29 -0800

    Add files via upload
    Changed to calculate based on input date, still need to implement timeframe restrictions

--- tumblr_caption_scraper.py
+++ tumblr_caption_scraper.py
@@ -8,7 +8,9 @@ import json
 import requests
 import sys
 import csv
+import time
 
+title = ""
 tag = ""
 limit = ""
 before = ""
@@ -21,12 +23,22 @@ def get_user_params():
 	global tag
 	global limit
 	global before
+	global title
 	while (tag == ""):
 		tag = raw_input("What tag would you like to scrape? ")
+	title = tag.replace(" ", "_")
+	tag = tag.replace(" ", "+")
 	while (limit == ""):
 		limit = raw_input("How many images would you like to scrape?  " )
 	while (before == ""):
-		before = raw_input("Would you like to include a time constraint? (to use current date, enter 0) ")
+		before = raw_input("Would you like to include a time constraint?\n(Note that this is the day that the scrape will start on. Enter in the format YYYY-MM-DD or enter 0 for current date) ")
+	if before != '0':
+		before = get_epoch_timestamp(before + " 23:59:59 GMT")
+
+def get_epoch_timestamp(date_time):
+	pattern = '%Y-%m-%d %H:%M:%S GMT'
+	epoch = int(time.mktime(time.strptime(date_time, pattern)))
+	return epoch 
 
 def scrape_image_captions():
 	global tag
@@ -34,12 +46,16 @@ def scrape_image_captions():
 	global before
 	global count
 	global urls
+	global title
 	url = 'https://api.tumblr.com/v2/tagged?tag='+ tag +'&limit='+ limit +'&api_key=p71Lr3B68z5H7AWMF95cbfZz08Kp7WexlgUwoEv2BmRGwowxVa'
 	if before != '0':
-		url += '&before=' + before
+		url += '&before=' + str(before)
 		latestTimestamp = before
 	urlLoaded = urllib2.urlopen(url)
 	data = json.load(urlLoaded)
+	if len(data['response']) == 0:
+		return
+	print count
 	for item in data['response']:
 		latestTimestamp = item['timestamp']
 		if 'image_permalink' in item:
@@ -62,13 +78,13 @@ def scrape_image_captions():
 		scrape_image_captions()
 
 def write_content_to_csv(content):
-	with open(tag + ".csv", 'a') as csvfile:
+	with open(title + ".csv", 'a') as csvfile:
 	    wr = csv.writer(csvfile, delimiter=',')
 	    for c in content:
 	    	wr.writerow(c)
 
 def write_urls_to_csv(urls):
-	with open(tag + "_urls.csv", 'a') as csvfile:
+	with open(title + "_urls.csv", 'a') as csvfile:
 		wr = csv.writer(csvfile, delimiter=',')
 		for u in urls:
 			wr.writerow(u)
